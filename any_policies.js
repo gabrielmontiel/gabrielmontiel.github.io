847410d8b4e5d645f0ebdbc3c108ba4077482f69
Gabriel Montiel <gabrielmontielp@gmail.com>
2025-01-26 03:34:21 -0500

    website any_policies working

--- any_policies.js
+++ any_policies.js
@@ -358,15 +358,16 @@ function cidr2bin(cidr){
     BinaryRoute = ipArray[0].slice(0,parseInt(ipArray[1]));
     return BinaryRoute
 }
+
+
 function Node(key, value){
     this.key = key
-    this.count = 0
     this.value = value  || "None"
     this.isEndOfWord = false // false by default, a green node means this flag is true
     this.children = {} // children are stored as Map, where key is the letter and value is a TrieNode for that letter 
-}
+    }
 
-class Trie{
+    class Trie{
     constructor(){
         this.root = new Node(null)
     }
@@ -378,12 +379,7 @@ class Trie{
             // if node doesn't have the current character as child, insert it
             if(current.children[character] === undefined){
                 current.children[character] = new Node(character)
-
-                
             }
-
-            //add to count
-            current.count += 1
             // move down, to insert next character
             current = current.children[character]  
         }
@@ -422,30 +418,7 @@ class Trie{
         }
         return match;
     }
-
-    get_likely_subnet(binary_address){
-        let current = this.root
-        let match = null;
-        level = 0
-        for(let character of binary_address){
-            if(current.children[character] === undefined){
-                return match;
-            }
-    
-            hosts = 2**(32-level)
-            percentage_hosts = current.count / hosts
-            if (percentage_hosts > 0.6){
-                match = current
-                return match
-            }
-            
-            //next character
-            current = current.children[character]
-            level += 1
-        }
     }
-    }
-
 
 
 const trie = new Trie();
@@ -459,12 +432,13 @@ function fillTrie(csvArray){
 
         //Route inserted to trie
         BinaryRoute = cidr2bin(csvArray[i][0]);
-        trie.insert(BinaryRoute,csvArray[i][1]);
-        TRIE_FILLED = true;
-        document.getElementById('trie_status')
-                .textContent="Loaded Route Table";
-        console.log("Trie filled")
+        trie.insert(BinaryRoute,csvArray[i][0]);
+        
     }
+    document.getElementById('trie_status')
+                .textContent="Loaded Route Table";
+    TRIE_FILLED = true;
+    console.log("Trie filled")
 }
 
 function create_tuple_rules(){
@@ -519,19 +493,19 @@ function create_tuple_rules(){
 
         //if no match, set address to exact address
         srcNet = trie.longest_prefix_match(cidr2bin(src))
-        srcNet = srcNet == undefined ? src : srcNet
+        srcNet = srcNet == undefined ? src : srcNet.value
         dstNet = trie.longest_prefix_match(cidr2bin(dst))
-        dstNet = dstNet == undefined ? dst : dstNet
+        dstNet = dstNet == undefined ? dst : dstNet.value
 
-        Net2tuplematch = `${srcNet.value}-${dstNet.value}`  
+        Net2tuplematch = `${dstNet},${port}`  
 
         if (newRules.has(Net2tuplematch)) {
-            newRules.get(Net2tuplematch).add(port)
+            newRules.get(Net2tuplematch).add(srcNet)
         }
         else {
-            const portSet = new Set();
-            portSet.add(port)
-            newRules.set(Net2tuplematch, portSet)
+            const sourceSet = new Set();
+            sourceSet.add(srcNet)
+            newRules.set(Net2tuplematch, sourceSet)
         }
     }
 
@@ -539,14 +513,36 @@ function create_tuple_rules(){
         console.log(element)
     });
 
-    // create set commands from newRules map
-    for (const [Net2tuple, ports] of newRules.entries()) {
-        srcdst = Net2tuple.split("-")
-        src = srcdst[0]
-        dst = srcdst[1]
+    var output  = ""
+    var index = 0
 
-        console.log(key, value);
+    // create set commands from newRules map
+    for (const [Net2tuple, sources] of newRules.entries()) {
+        index += 1 
+        dstport = Net2tuple.split(",")
+        dst = dstport[0]
+        port = dstport[1]
+
+        joinedsources = Array.from(sources).join(",")
+
+
+        template = `set rulebase security rules "Allow flow ${index}" to any
+set rulebase security rules "Allow flow ${index}" from any
+set rulebase security rules "Allow flow ${index}" source [ ${joinedsources} ]
+set rulebase security rules "Allow flow ${index}" destination [ ${dst} ]
+set rulebase security rules "Allow flow ${index}" source-user any
+set rulebase security rules "Allow flow ${index}" category any
+set rulebase security rules "Allow flow ${index}" application any
+set rulebase security rules "Allow flow ${index}" service any
+set rulebase security rules "Allow flow ${index}" source-hip any
+set rulebase security rules "Allow flow ${index}" destination-hip any
+set rulebase security rules "Allow flow ${index}" action allow
+set rulebase security rules "Allow flow ${index}" log-setting default`
+        
+        output+= template
       }
+    const element = document.getElementById("output")
+    element.innerHTML = `${output}`
 }
 
 function supernet(ip_address, mask){
